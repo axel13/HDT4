0878fb40b31029e0e8624714d67287eaf504f816
Axel Mazariegos <axel.licardie@icloud.com>
2016-08-22 22:19:54 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * Axel Mazariegos 131212
@@ -16,8 +10,28 @@ public class main {
         String Archivo;
         
         try{
-            System.out.println ("Nombre del archivo, incluir extension '.txt' " );
+            System.out.println ("Nombre del archivo, incluir extension (.txt): " );
             Archivo = texto.next();
+            
+            BufferedReader br = new BufferedReader(new FileReader(Archivo));
+            try {
+                String line = br.readLine();
+
+                while (line != null) {
+                    lines.add(line);
+                    line = br.readLine();
+                }    
+            } finally {
+            br.close();
+            }
+            
+            
+            
         }
+        
+        catch(Exception e)
+		{
+			System.out.println("no se encontro archivo solicitado/ \nno esta bien definida la operacion");
+		}
     }
 }
